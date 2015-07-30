dc7a5ba6292f999fbb63016a846186167465d9ae
Vinu Devarajan <vinu.chennai.91@gmail.com>
2015-07-30 23:12:03 +0530

    Add validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,10 @@ class User < ActiveRecord::Base
 
   has_many :micro_posts
 
+  before_save { self.email = email.downcase }
+
+  validates :name, presence: true, length: { maximum: 50 }
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 255 }, format: VALID_EMAIL_REGEX
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150730094441) do
+ActiveRecord::Schema.define(version: 20150730173749) do
 
   create_table "micro_posts", force: :cascade do |t|
     t.text     "content"
@@ -26,5 +26,7 @@ ActiveRecord::Schema.define(version: 20150730094441) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
