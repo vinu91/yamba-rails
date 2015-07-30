8fab7974f1ed67af69cfff6c69522f5724ad904b
Vinu Devarajan <vinu.chennai.91@gmail.com>
2015-07-30 22:01:37 +0530

    Use named routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,10 @@ Rails.application.routes.draw do
   # Root of the site
   root 'static_pages#home'
 
-  get 'static_pages/home'
-
-  get 'static_pages/help'
-
-  get 'static_pages/about'
-
-  get 'static_pages/contact'
+  get 'home'    => 'static_pages#home'
+  get 'help'    => 'static_pages#help'
+  get 'about'   => 'static_pages#about'
+  get 'contact' => 'static_pages#contact'
 
   resources :micro_posts
   resources :users
