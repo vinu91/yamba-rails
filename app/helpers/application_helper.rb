abea43d113ae94a294b67c651daf94be138bb18b
Vinu Devarajan <vinu.chennai.91@gmail.com>
2015-07-30 20:51:45 +0530

    Move page title to helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  def full_title(page_title = '')
+    base_title = "YAMBA"
+    if page_title.present?
+      "#{page_title} | #{base_title}"
+    else
+      base_title
+    end
+  end
+
 end
