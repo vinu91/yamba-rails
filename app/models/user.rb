2314bdddc88a8653bd47bbf7c96be767b3ec9a0a
Vinu Devarajan <vinu.chennai.91@gmail.com>
2015-07-30 23:30:01 +0530

    Add password to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,14 @@ class User < ActiveRecord::Base
 
   has_many :micro_posts
 
-  before_save { self.email = email.downcase }
+  before_save { self.email.downcase! }
 
   validates :name, presence: true, length: { maximum: 50 }
 
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 255 }, format: VALID_EMAIL_REGEX
+
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
+
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,8 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
 
   def setup
-    @user = User.new(name: "Example User", email: "user@example.com")
+    @user = User.new(name: "Example User", email: "user@example.com",
+                     password: "foobar", password_confirmation: "foobar")
   end
 
   test "should be valid" do
@@ -47,7 +48,7 @@ class UserTest < ActiveSupport::TestCase
 
   test "email validation should reject invalid addresses" do
     invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
-                           foo@bar_baz.com foo@bar+baz.com]
+                           foo@bar_baz.com foo@bar+baz.com foo@bar..com]
     invalid_addresses.each do |invalid_address|
       @user.email = invalid_address
       assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
@@ -59,6 +60,23 @@ class UserTest < ActiveSupport::TestCase
     duplicate_user.email = duplicate_user.email.upcase
     @user.save
     assert_not duplicate_user.valid?
+  end
+
+  test "email addresses should be downcased" do
+    mixed_case_email = "MiXeDCase@EXAMPLE.CoM"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email, @user.reload.email
+  end
+
+  test "password should not be blank" do
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
+  end
+
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
   end
 
 end
