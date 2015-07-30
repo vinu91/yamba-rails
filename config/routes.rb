b61d74054201202e4ac0416a07eaf061e0e96915
Vinu Devarajan <vinu.chennai.91@gmail.com>
2015-07-30 20:36:27 +0530

    Add Static Pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 Rails.application.routes.draw do
+  # Root of the site
+  root 'static_pages#home'
+
+  get 'static_pages/home'
+
+  get 'static_pages/help'
+
+  get 'static_pages/about'
+
+  get 'static_pages/contact'
 
   resources :micro_posts
   resources :users
 
-  # Root of the site
-  root 'users#index'
 
 end
